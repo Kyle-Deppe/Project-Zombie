2b68c018bf2d9315f3336b3a714cbca56401514e
Joel Abshier <abshierjoel@gmail.com>
2017-10-22 21:56:59 -0500

    Created Eclipse Project
    Created an eclipse project to manage. You can import it using
    File->Import

---
+++ src/ece3220_final.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ECE 3220: Final Project
+// Author      : Joel Abshier, Kyle Depp, Tsiania Hughes
+// Version     : 0.0.1
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "The Ultimate Final Project" << endl;
+	return 0;
+
+}
